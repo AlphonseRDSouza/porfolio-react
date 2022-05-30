655cfdeb9801ae597c66c1374482e25c7aeedcea
alphonso <appu007dsouza@gmail.com>
2022-05-30 21:42:36 +0530

    Design: Header socials with react-icons lib added

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { ImStackoverflow } from "react-icons/im";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://stackoverflow.com/users/1057814/alphonse-r-dsouza"
+        target="_blank"
+      >
+        <ImStackoverflow />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
