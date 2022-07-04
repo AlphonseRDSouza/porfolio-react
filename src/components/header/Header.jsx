e8c699da4f3390c045c5f990ec7d2540ff13caf9
AlphonseRDsouza <appu007dsouza@gmail.com>
2022-07-05 00:25:11 +0530

    Header.js 'header__container' wrapping fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,20 +8,21 @@ import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
-      <div className="container header__container"></div>
-      <h5>Hello I'm</h5>
-      <h1>Alphonse R. D'souza</h1>
-      <h5 className="text-light">
-        Lead Frontend and mobile application developer
-      </h5>
-      <CTA />
-      <HeaderSocials />
-      <div className="me">
-        <img src={ME} alt="me" />
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Alphonse R. D'souza</h1>
+        <h5 className="text-light">
+          Lead Frontend and mobile application developer
+        </h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
-      <a href="#contact" className="scroll__down">
-        Scroll Down
-      </a>
     </header>
   );
 };
