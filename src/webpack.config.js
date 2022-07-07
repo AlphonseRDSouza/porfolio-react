c6f57b1c45bb9afcf9bb821f5fcad6c05c8613db
alphonso <alphonse.dsouza@indpro.se>
2022-07-08 04:30:49 +0530

    Webpack configuration updated

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = (env, argv) => ({
     context: path.join(process.cwd(), 'src'),
   
     output: {
-      publicPath: (argv || {}).mode === 'production' ? '/portfolio-react' : '/',
+      // publicPath: (argv || {}).mode === 'production' ? '/portfolio-react' : '/',
       path: path.join(process.cwd(), 'dist'),
       filename: 'scripts/[name].[hash].js',
     },
